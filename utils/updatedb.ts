87724fd009388bc10213dfaa2244f4ab78337f99
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-09-17 19:44:53 -0400

    start schedule displays

--- utils/updatedb.ts
+++ utils/updatedb.ts
@@ -1,5 +1,7 @@
-import { doc, setDoc, deleteDoc } from "firebase/firestore";
+import { doc, setDoc, deleteDoc, Timestamp } from "firebase/firestore";
 import { db } from "./firebase";
+
+const freqs = [13148730000, 5259492000, 2629746000, 1209600000, 604800000]
 
 export const addNew = async (
   user: any,
@@ -17,6 +19,8 @@ export const addNew = async (
     tone: tone,
     freq: freq,
     intention: intention,
+    current: Timestamp.now(),
+    next: Timestamp.fromMillis(Date.now() + freqs[parseInt(freq)-1])
   });
 };
 
