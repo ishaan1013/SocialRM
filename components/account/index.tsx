9c5ab7f8229b6242dea70e2ace06cd7e1cbee82a
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-09-17 02:42:13 -0400

    firebase kinda works

---
+++ components/account/index.tsx
@@ -0,0 +1,24 @@
+import { FaGoogle } from 'react-icons/fa'
+
+interface Props {
+    signIn: () => void
+}
+
+const Account:React.FC<Props> = ({signIn}) => {
+
+    return (
+        <main className="w-full h-full flex items-center justify-center px-6">
+            <div className="w-full sm:w-auto p-8 rounded-lg border-2 border-slate-300 bg-white flex flex-col items-center justify-start">
+                <h1 className="text-slate-700 font-bold text-lg mb-6">Login To Your SocialRM</h1>
+                <button
+                onClick={() => signIn()}
+                className="w-full sm:w-auto text-sm sm:text-base px-0 sm:px-10 py-2 flex justify-center items-center rounded-lg bg-purple-500 hover:bg-purple-500/90 text-white font-semibold duration-200">
+                    <FaGoogle className="text-white mr-2" />
+                    Log In With Google
+                </button>
+            </div>
+        </main>
+    )
+}
+
+export default Account
---
+++ utils/firebase.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcYLI-eEpHMEQroY7fkVqutRLy5EWYz3k",
+  authDomain: "socialrm-htn.firebaseapp.com",
+  projectId: "socialrm-htn",
+  storageBucket: "socialrm-htn.appspot.com",
+  messagingSenderId: "454954297243",
+  appId: "1:454954297243:web:a7e49feda7ced9a830233e"
+};
+
+const app = initializeApp(firebaseConfig);
