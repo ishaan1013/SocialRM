0d36f4ea88b9827b83c22456b031fbf83760c71c
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-09-17 16:13:44 -0400

    sidebar on desktop

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -16,7 +16,7 @@ const Nav:React.FC<Props> = ({user, auth}) => {
 
     return (
         <>
-            <div className="w-full p-4 border-b-[1px] border-slate-200 flex justify-between items-center text-slate-700">
+            <div className="md:hidden w-full p-4 border-b-[1px] border-slate-200 flex justify-between items-center text-slate-700">
                 <Link href="/">
                     <h1 className="font-bold text-lg cursor-pointer">SocialRM</h1>
                 </Link>
@@ -27,7 +27,7 @@ const Nav:React.FC<Props> = ({user, auth}) => {
                 </button>
             </div>
             {isOpen && 
-            <nav className="h-screen w-screen fixed top-0 left-0 flex flex-col justify-start pt-24 px-6 bg-gradient-to-br from-purple-700 to-purple-500 z-50">
+            <nav className="md:hidden h-screen w-screen fixed top-0 left-0 flex flex-col justify-start pt-24 px-6 bg-gradient-to-br from-purple-700 to-purple-500 z-50">
                 <button
                 className="absolute top-6 right-6"
                 onClick={() => setIsOpen(false)}
@@ -67,6 +67,49 @@ const Nav:React.FC<Props> = ({user, auth}) => {
                 </button>
             </nav>
             }
+
+            {/* computer size below */}
+            <nav className="hidden md:flex h-screen w-80 fixed top-0 left-0 flex-col justify-start pt-24 px-6 bg-gradient-to-br from-purple-700 to-purple-500 z-50">
+                <h1 className="absolute top-7 left-6 font-bold text-[1.35rem]  text-white">SocialRM</h1>
+
+                <button
+                className="absolute top-6 right-6"
+                onClick={() => setIsOpen(false)}
+                >
+                    <IoClose className="w-9 h-9 p-2 rounded-lg bg-white/10 hover:bg-white/20 duration-200 text-white" />
+                </button>
+
+                <Link href="/">
+                    <div
+                    className="cursor-pointer w-full p-2 duration-200 bg-transparent rounded-lg hover:bg-white/10 mb-4 font-semibold text-white text-start"
+                    >
+                        Dashboard
+                    </div>
+                </Link>
+
+                <Link href="/schedule">
+                    <div
+                    className="cursor-pointer w-full p-2 duration-200 bg-transparent rounded-lg hover:bg-white/10 mb-6 font-semibold text-white text-start"
+                    >
+                        Schedule
+                    </div>
+                </Link>
+
+                <div className="flex items-center w-full px-2">
+                    <CgProfile className="w-8 h-8 text-white mr-3" />
+                    <div>
+                        <p className="text-white font-semibold -mb-1">{user ? user.displayName : ""}</p>
+                        <p className="text-white/70 font-base text-sm">{user ? user.email : ""}</p>
+                    </div>
+                </div>
+
+                <button
+                className="w-full p-2 duration-200 bg-white rounded-lg hover:bg-purple-100/90 mt-12 font-bold text-purple-600 text-center"
+                onClick={() => signOutFunc(auth)}
+                >
+                    Sign Out
+                </button>
+            </nav>
         </>
     )
 }
